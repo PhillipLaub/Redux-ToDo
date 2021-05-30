8753ed23f7908be67cbc3cfb22aa5366156e14e1
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2021-05-30 11:53:26 -0700

    input component created, needs styling

---
+++ src/components/Input.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./input.css";
+
+function Input() {
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    console.log("addtodo: " + input);
+  };
+  return (
+    <div className="input">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTodo}>Add!</button>
+    </div>
+  );
+}
+
+export default Input;
